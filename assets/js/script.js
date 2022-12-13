d652745f2c8b1f909d31e09b5b1b20c5c9c7ff34
Alyssa Red <redalyssa636@gmail.com>
2022-12-12 22:38:00 -0500

    you bad person you

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,97 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+timeDisplayEl = $("#currentDay")
+tzDisplayEl = $("#currentTZ")
+eventsContainerEl = $("#events-container")
+clearBtnEl = $("#clearBtn");
+
+
+$(function () {  
+  // Function saves events to localStorage when the save button is clicked
+  function saveEvent(e) {
+    e.preventDefault();
+    var hourToSave = $(this).parents("div.time-block").attr("id");
+    var textToSave = $(this).siblings("textarea").val();
+    localStorage.setItem(hourToSave, textToSave);
+  };
+  
+  // Save button event listener binding
+  eventsContainerEl.on("click", ".saveBtn", saveEvent)
+
+
+  function checkTime() {
+    var utc = window.dayjs_plugin_utc;
+    var timezone = window.dayjs_plugin_timezone;
+    dayjs.extend(utc)
+    dayjs.extend(timezone)
+    var timezoneToSet = dayjs.tz.guess()
+    currentHour = dayjs().format('H', timezoneToSet);
+    for (i = 8; i < 18; i++) {
+      var hourToChange =  "#hour-" + i;
+      if (currentHour == i) {
+         $(hourToChange).removeClass("future");
+         $(hourToChange).removeClass("past");
+         $(hourToChange).addClass("present");
+      } else if (currentHour < i) {
+        $(hourToChange).removeClass("present");
+        $(hourToChange).removeClass("past");
+        $(hourToChange).addClass("future");
+      } else if (currentHour > i) {
+        $(hourToChange).removeClass("present");
+        $(hourToChange).removeClass("future");
+        $(hourToChange).addClass("past");
+      }
+    };
+  };
+
+  
+  
+  // Function verifies if anything is saved and localStorage and displays any saved events onto timeblocks textarea
+  function getSavedEvents() {
+    for (i = 8; i < 18; i++) {
+      var hourToGet =  "hour-" + i;
+      textToGet = localStorage.getItem(hourToGet);
+      if (textToGet === null) {
+        eventsContainerEl.children("#" + hourToGet).children("textarea").html("");
+      } else {
+          eventsContainerEl.children("#" + hourToGet).children("textarea").html(textToGet);
+      }
+    }
+  };
+  
+  // Fucntion displays date and time based on user's system timezone
+  function displayTime() {
+    var utc = window.dayjs_plugin_utc;
+    var timezone = window.dayjs_plugin_timezone;
+    dayjs.extend(utc)
+    dayjs.extend(timezone)
+    // var timezoneToSet = dayjs.tz.guess()
+    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+    // tzDisplayEl.text("Your current system timezone is: " + timezoneToSet);
+  }
+
+  // Clear events from localStorage
+  function clearSchedule() {
+    confirmModal = confirm("Are you sure you want to clear your schedule? This action is not reversible!");
+    console.log(confirmModal);
+    if (confirmModal) {
+      for (i = 8; i < 18; i++) {
+        var itemToRemove =  "hour-" + i;
+        localStorage.removeItem(itemToRemove);
+      };
+    getSavedEvents();
+    };
+  };
+
+  clearBtnEl.click(clearSchedule);
+  
+  setInterval(displayTime, 1000);
+  setInterval(checkTime, 10000);
+  
+  function init() {
+    getSavedEvents();
+    checkTime();
+  }
+  
+  init();
+
+});
